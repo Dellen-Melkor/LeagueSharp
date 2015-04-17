0b3e95707035b7da4166216744c6d83c1067d2e8
HaydariGeceler <batudatorlol@gmail.com>
2015-04-17 15:08:42 +0300

    HaydariGeceler_Cici_Wipi_ENG

--- HaydariGeceler_Cici_Wipi_ENG/Program.cs
+++ HaydariGeceler_Cici_Wipi_ENG/Program.cs
@@ -25,8 +25,10 @@ namespace HaydariGeceler_cici_wipi_ENG
                 "<font color = \"#ff052b\">HaydariGeceler cici wipi</font>  <font color = \"#fcdfff\">Loaded  </font> ");
 
             haydarigeceler = new LeagueSharp.Common.Menu("HaydariGeceler cici wipi", "", true);
-            var press =
-                haydarigeceler.AddItem(new MenuItem("GGyaz", "Write GG").SetValue(new KeyBind(37, KeyBindType.Press)));
+            var press = haydarigeceler.AddItem(new MenuItem("GGyaz", "Write GG").SetValue(new KeyBind(37, KeyBindType.Press)));
+            var press2 = haydarigeceler.AddItem(new MenuItem("WPyaz", "Write WP").SetValue(new KeyBind(39, KeyBindType.Press)));
+            var press3 = haydarigeceler.AddItem(new MenuItem("XDyaz", "Write XD").SetValue(new KeyBind(40, KeyBindType.Press)));
+            var press4 = haydarigeceler.AddItem(new MenuItem("PNSciz", "Draw Penis").SetValue(new KeyBind(96, KeyBindType.Press)));
             haydarigeceler.AddItem(new MenuItem("İnfo", "Writed By HaydariGeceler, thanks to everyone for the support:)"));
             haydarigeceler.AddToMainMenu();
 
@@ -47,6 +49,78 @@ namespace HaydariGeceler_cici_wipi_ENG
                         duramk = true;
                         gameTime1 = Game.Time + 1;
                         
+                    }
+                if (Game.Time > gameTime1)
+                {
+                    duramk = false;
+                }
+            };
+            press2.ValueChanged += delegate(object sender, OnValueChangeEventArgs EventArgs)
+            {
+                if (haydarigeceler.Item("WPyaz").GetValue<KeyBind>().Active)
+                    if (duramk == false)
+                    {
+
+                        Game.Say("/all                           ");
+                        Game.Say("/all  ##         ##   ####### ");
+                        Game.Say("/all  ##  ##  ##   ##      ##");
+                        Game.Say("/all  ##  ##  ##   ##      ##");
+                        Game.Say("/all  ##  ##  ##   ####### ");
+                        Game.Say("/all  ##  ##  ##   ##       ");
+                        Game.Say("/all  ##  ##  ##   ##       ");
+                        Game.Say("/all   ###  ###    ##      ");
+
+                        duramk = true;
+                        gameTime1 = Game.Time + 1;
+
+                    }
+                if (Game.Time > gameTime1)
+                {
+                    duramk = false;
+                }
+            };
+            press3.ValueChanged += delegate(object sender, OnValueChangeEventArgs EventArgs)
+            {
+                if (haydarigeceler.Item("XDyaz").GetValue<KeyBind>().Active)
+                    if (duramk == false)
+                    {
+
+                        Game.Say("/all                      ");
+                        Game.Say("/all  ##     ##  #######");
+                        Game.Say("/all    ##   ##   ##         ##");
+                        Game.Say("/all     ## ##    ##         ##");
+                        Game.Say("/all     ###      ##         ##");
+                        Game.Say("/all     ## ##    ##         ##");
+                        Game.Say("/all    ##   ##   ##         ##");
+                        Game.Say("/all  ##     ##  #######");
+
+                        duramk = true;
+                        gameTime1 = Game.Time + 1;
+
+                    }
+                if (Game.Time > gameTime1)
+                {
+                    duramk = false;
+                }
+            };
+            press4.ValueChanged += delegate(object sender, OnValueChangeEventArgs EventArgs)
+            {
+                if (haydarigeceler.Item("PNSciz").GetValue<KeyBind>().Active)
+                    if (duramk == false)
+                    {
+
+                        Game.Say("/all          ____");
+                        Game.Say("/all        / /     7");
+                        Game.Say("/all       (__,__/\\ ");
+                        Game.Say("/all        \\         \\ ");
+                        Game.Say("/all         \\         \\ ");
+                        Game.Say("/all       __\\         \\__");
+                        Game.Say("/all      (     \\            )");
+                        Game.Say("/all       \\___\\_____/  ");
+
+                        duramk = true;
+                        gameTime1 = Game.Time + 1;
+
                     }
                 if (Game.Time > gameTime1)
                 {
--- HaydariGeceler_Cici_Wipi_ENG/Properties/AssemblyInfo.cs
+++ HaydariGeceler_Cici_Wipi_ENG/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d1036997-e316-4530-9365-67d5b505ce74")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.1.1")]
+[assembly: AssemblyFileVersion("1.0.1.1")]
